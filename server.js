41811978517d713fffc42e531aa02fa4a4043100
Aman Mishra <ashu18aman@gmail.com>
2026-01-15 00:58:58 +0530

    fix: db start 1st

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+const startServer = async () => {
+  await connectDB(); // 🔥 MUST HAPPEN FIRST
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
