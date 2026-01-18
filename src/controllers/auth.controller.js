8258bcf87240ed8f15f8af40b51c331dda593e8e
Aman Mishra <ashu18aman@gmail.com>
2026-01-18 17:19:06 +0530

    Added role in login response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -67,12 +67,18 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "1d" },
     );
 
     res.status(200).json({
       message: "Login successful",
       token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     });
   } catch (error) {
     console.error("LOGIN ERROR:", error);
